89b50ab2eccc02ef4c22c825bba9ca64ff56706b
rimonnnn <rimonseha123@gmail.com>
2026-03-31 17:34:24 +0200

    first commit

--- bin/test_dart_1.dart
+++ bin/test_dart_1.dart
@@ -1,5 +1,23 @@
-import 'package:test_dart_1/test_dart_1.dart' as test_dart_1;
+int add(int x, int y) {
+  return x * y;
+}
 
-void main(List<String> arguments) {
-  print('Hello world: ${test_dart_1.calculate()}!');
+void greating({String name = "Rimon", int age = 0}) {
+  print("Hello $name your age is $age years old");
+}
+
+String joinWords(String word1, [String? word2]) {
+  if (word2 != null) {
+    return "$word1  $word2";
+  }
+  return "$word1";
+}
+
+int multibly(int x, int y) => x * y;
+
+void main() {
+  greating(name: "Rimon", age: 21);
+  print(add(5, 6));
+  print(joinWords(joinWords("Hello", "Rimon")));
+  print(multibly(6, 6));
 }
